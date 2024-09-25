deb7cce992cdbcf828905ce912337d4ab028c638
Robson Renan Da Silva <robson.rsilva@tecadi.com.br>
2024-09-25 13:17:05 -0300

    ajustando a tabnav

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,29 +1,31 @@
 export default class TabNav {
-  constructor (menu, content){
+  constructor(menu, content) {
     this.tabMenu = document.querySelectorAll(menu);
     this.tabContent = document.querySelectorAll(content);
     this.activeClass = 'ativo';
-
   }
-  
-    activeTab(index) {
-      this.tabContent.forEach((section) => {
-        section.classList.remove(this.activeClass);
-      });
-      const direcao = this.tabContent[index].dataset.anime;
-      this.tabContent[index].classList.add(this.activeClass, direcao);
-    }
 
-    addTabNavEvent(){
-      this.tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => this.activeTab(index));
-      });
-    }
+  // Ativa a tab de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
 
-    init(){
-      if(tabMenu.length && tabContent.length) {
-        this.activeTab(0);
-        this.addTabNavEvent()
+  // Adiciona os eventos nas tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
     }
   }
 }
